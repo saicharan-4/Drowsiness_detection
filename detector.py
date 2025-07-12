eb789c49f4ef9a499e994848f80de0eda50c4cfd
G Saicharan Reddy <115097933+saicharan-4@users.noreply.github.com>
2025-07-12 23:26:56 +0530

    Create detector.py

---
+++ detector.py
@@ -0,0 +1,10 @@
+from scipy.spatial import distance as dist
+import cv2
+import dlib
+
+def eye_aspect_ratio(eye):
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+    C = dist.euclidean(eye[0], eye[3])
+    ear = (A + B) / (2.0 * C)
+    return ear
